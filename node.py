fa4e0a06a9f82c61996e1ea2298445df2a93db7d
root <root@ctedserver-HP-ProOne-600-G1-AiO.(none)>
2015-09-22 08:02:33 -0400

    Added upload class to node.py

--- node.py
+++ node.py
@@ -77,6 +77,27 @@ class marketplace_aws:
 			syslog.syslog("AALU: not enough params");
 
             # raise web.seeother('/upload')
+class upload:
+	def __init_(self):
+		pass
+
+	def POST(self):
+	    syslog.syslog("AALU: In Upload POST")
+            data = web.input(myfile={})
+            syslog.syslog("AALU: Data " + str(data))
+            filedir = '/home/cted-server/FilesToBeUploadedAWS' 
+            if 'myfile' in data: 
+                filepath=data.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+                filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+                syslog.syslog("AALU: File Path " + filedir + '/' + filename)
+                fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
+                fout.write(data.myfile.file.read()) # writes the uploaded file to the newly created file.
+                fout.close() # closes the file, upload complete.
+                # thread = uploader.file_uploader('http://128.122.140.120:8888/ivr_server', '', filedir + '/' + filename)
+                thread = uploader.file_uploader('http://ec2-54-93-162-141.eu-central-1.compute.amazonaws.com:8080/ivr_server', '', filedir + '/' + filename)
+                thread.start()
+                # thread.join()
+            # raise web.seeother('/upload')
 
 class marketplace_real:
 	def __init_(self):
