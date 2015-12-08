b2acf9b97688c1a6a09d241eee485b2878a9d25c
talalahmad <ahmad@cs.nyu.edu>
2015-12-08 03:34:31 -0500

    very last try

--- starter.py
+++ starter.py
@@ -138,7 +138,7 @@ class bts_client3:
 		 	data_to_be_sent['i'] = from_name;
 		 	data_to_be_sent['t'] = "NEW";
 		 	data_to_be_sent['d'] = from_number;
-			syslog.syslog("BALU: uid=%s and time=%s" %(identity,str(time.time())))
+			syslog.syslog("BALU: uid=%s and time=%s" %(from_name,str(time.time())))
 		 	thread[i] = get.get('http://'+nodes[i]+':8080/server','',data_to_be_sent);
 		 	thread[i].start();
 		 	syslog.syslog("BALU: Node selected is %s" %node);
