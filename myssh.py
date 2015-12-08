2e39f3c8fb2230bf2154ad5033c7dfefdc618069
talalahmad <ahmad@cs.nyu.edu>
2015-12-08 10:29:02 -0500

    hoping this is final

--- myssh.py
+++ myssh.py
@@ -1,4 +1,5 @@
 import paramiko
+#from scp import SCPClient
 
 def connect_after(ip, key, how_many):
 	k = paramiko.RSAKey.from_private_key_file("/home/ec2-user/"+key)
@@ -31,3 +32,13 @@ def connect_before(ip, key, how_many):
 		print( "Errors")
 		print stderr.read()
 	c.close()
+
+# def download_files(ip, key, filename):
+# 	k = paramiko.RSAKey.from_private_key_file("/home/talal/Dropbox/aws/"+key)
+# 	c = paramiko.SSHClient()
+# 	c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# 	print "connecting"
+# 	c.connect( hostname = ip, username = "ec2-user", pkey = k )
+# 	print "connected"
+# 	scp = SCPClient(c.get_transport());
+# 	scp.get('/home/ec2-user/'+filename,'/home/talal/'+key.split(',')[0])
